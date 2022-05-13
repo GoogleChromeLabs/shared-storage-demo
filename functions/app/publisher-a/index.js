add5058c0c90a9e65cd0f3024708e54c9887199c
kevinkiklee <kevinkiklee@gmail.com>
2022-05-13 15:57:45 -0400

    Setup skeleton

---
+++ functions/app/publisher-a/index.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const express = require('express');
+const functions = require('firebase-functions');
+const setupView = require('../helpers/setup-view');
+
+// Read the dev or prod URLs to be used
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+
+// Setup app and view
+const app = setupView(express(), 'publisher-a');
+
+// Setup root route
+app.get('/', (req, res) => {
+  const { DEMO_HOME_URL, PUBLISHER_A_URL, PUBLISHER_B_URL, ADTECH_URL } = process.env;
+
+  res.render('index', {
+    demoHomeUrl: DEMO_HOME_URL,
+    publisherAUrl: PUBLISHER_A_URL,
+    publisherBUrl: PUBLISHER_B_URL,
+    adtechUrl: ADTECH_URL,
+  });
+});
+
+exports.publisherA = functions.https.onRequest(app);
